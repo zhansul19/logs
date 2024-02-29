c9f284b35ae77ec91453159e88c2bf93027cc857
zhansul19 <zhansultanjan@gmail.com>
2024-02-29 12:36:41 +0600

    logger update, cascade update

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(
     filename='app.log',  # Specify the file where logs will be saved
     level=logging.INFO,  # Set the logging level
-    format='%(asctime)s - %(levelname)s - %(user)s - %(message)s - %(table)s',  # Specify the log message format
+    format='%(asctime)s - %(levelname)s - %(user)s - %(message)s - %(table)s - %(action)s',  # Specify the log message format
     datefmt='%Y-%m-%d %H:%M:%S',  # Specify the date format
     encoding='utf-8'
 )
