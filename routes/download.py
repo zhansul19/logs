830149e1cba7aabe31305a782e726e07877a2003
zhansul19 <zhansultanjan@gmail.com>
2024-03-01 16:46:28 +0500

    download itab search

--- routes/download.py
+++ routes/download.py
@@ -5,6 +5,7 @@ from auth import get_current_user
 from sqlalchemy.orm import Session
 from sqlalchemy import String, cast, func, and_
 import pandas as pd
+import re
 
 
 router = APIRouter()
@@ -30,23 +31,25 @@ async def download_excel(requestname: str, tagname: str, value: str, current_use
                 cast(User.username, String).contains(value)).all()
             logging.info(f"{value}", extra={'user': current_user, 'table': 'itap',
                                             'action': 'скачал поиск пользователя по username'})
-        elif tagname == "search":
-            log_entries = db.query(Log.username, User.email, Log.request_body, Log.request_rels, Log.date,
-                                   Log.approvement_data, Log.obwii, Log.depth_, Log.limit_).join(User,
-                                                                                                 Log.username == User.username).filter(
-                cast(Log.request_body, String).contains(value)).all()
+        elif tagname == "username_partial":
+            log_entries = (
+                db.query(Log.username, User.email, Log.request_body, Log.request_rels, Log.date, Log.approvement_data,
+                         Log.obwii, Log.depth_, Log.limit_)
+                .join(User, Log.username == User.username)
+                .filter(cast(Log.username, String)
+                        .contains(value)).all())
+            logging.info(f"{value}", extra={'user': current_user, 'table': 'itap',
+                                            'action': 'скачал поиск пользователя по username'})
+        elif tagname == "iin":
+            log_entries = (
+                db.query(Log.username, User.email, Log.request_body, Log.request_rels, Log.date, Log.approvement_data,
+                         Log.obwii, Log.depth_, Log.limit_)
+                .join(User, Log.username == User.username)
+                .filter(cast(Log.request_body, String)
+                        .contains(value)).all())
             logging.info(f"{value}", extra={'user': current_user, 'table': 'itap',
                                             'action': 'скачал поиск по иин'})
 
-        else:
-            raise HTTPException(status_code=404, detail=f"Bad Request")
-    elif requestname == "search":
-        req_type = 1
-        if tagname == "value":
-            log_entries = db.query(Log.username, User.email, Log.request_body, Log.request_rels, Log.date,
-                                   Log.approvement_data, Log.obwii, Log.depth_, Log.limit_).join(User,
-                                                                                                 Log.username == User.username).filter(
-                cast(Log.request_body, String).contains(value)).all()
         else:
             raise HTTPException(status_code=404, detail=f"Bad Request")
     elif requestname == "user_log":
@@ -55,6 +58,14 @@ async def download_excel(requestname: str, tagname: str, value: str, current_use
             log_entries = db.query(UsersLog.username, Cascade.name, UsersLog.message, UsersLog.debug_level,
                                    UsersLog.time).join(Cascade, UsersLog.username == Cascade.email).filter(
                 UsersLog.username == value).all()
+            logging.info(f"{value}", extra={'user': current_user, 'table': 'users_log_cascade',
+                                            'action': 'скачал поиск пользователя по username'})
+        elif tagname == "username_partial":
+            log_entries = (
+                db.query(UsersLog.username, Cascade.name, UsersLog.message, UsersLog.debug_level, UsersLog.time)
+                .join(Cascade, UsersLog.username == Cascade.email)
+                .filter(cast(UsersLog.username, String)
+                        .contains(value)).all())
             logging.info(f"{value}", extra={'user': current_user, 'table': 'users_log_cascade',
                                             'action': 'скачал поиск пользователя по username'})
         elif tagname == "message":
@@ -110,10 +121,22 @@ async def download_excel_dossie_log(tag: str, value: str, current_user: str = De
     if tag == "username":
         log_entries = db.query(DossieLog).filter(DossieLog.user_name == value).all()
         logging.info(f"{value}",extra={'user': current_user, 'table': 'dossie_log', 'action': 'скачал поиск пользователя по user_name'})
+    elif tag == "username_partial":
+        log_entries = (db.query(DossieLog)
+                       .filter(cast(DossieLog.user_name, String)
+                               .contains(value))
+                       .order_by(DossieLog.log_time.desc()).all())
+        logging.info(f"{value}", extra={'user': current_user, 'table': 'dossie_log',
+                                        'action': 'скачал поиск пользователя по user_name'})
     elif tag == "fullname":
         value=value.upper()
-        log_entries = db.query(DossieLog).filter(func.concat(DossieLog.lname,' ',DossieLog.fname,' ',DossieLog.mname).contains(value)).all()
+        log_entries = db.query(DossieLog).filter(func.concat(DossieLog.lname,' ',DossieLog.fname,' ',DossieLog.mname).contains(value)).order_by(DossieLog.log_time.desc()).all()
         logging.info(f"{value}",extra={'user': current_user, 'table': 'dossie_log', 'action': 'скачал поиск пользователя по фио'})
+    elif tag == "fullname_full":
+        value=value.upper()
+        log_entries = db.query(DossieLog).filter(func.concat(DossieLog.lname,' ',DossieLog.fname,' ',DossieLog.mname).contains(value)).order_by(DossieLog.log_time.desc()).all()
+        logging.info(f"{value}",extra={'user': current_user, 'table': 'dossie_log', 'action': 'скачал поиск пользователя по фио'})
+
     elif tag == "action":
         log_entries = db.query(DossieLog).filter(DossieLog.action.like(f'%{value}%')).all()
         logging.info(f"{value}",extra={'user': current_user, 'table': 'dossie_log', 'action': 'скачал поиск по иин'})
@@ -147,6 +170,9 @@ async def download_excel_dossie_log(tag: str, value: str, current_user: str = De
 async def get_dossie_fullname_log_entries(lname: str = Query(None),
                                           mname: str = Query(None),
                                           fname: str = Query(None),
+                                          full_lname: str = Query(None),
+                                          full_mname: str = Query(None),
+                                          full_fname: str = Query(None),
                                           current_user: str = Depends(get_current_user),
                                           db: Session = Depends(get_db2)):
 
@@ -158,6 +184,16 @@ async def get_dossie_fullname_log_entries(lname: str = Query(None),
         filter_conditions.append(DossieLog.action.like(f'%{mname}%'))
     if lname:
         filter_conditions.append(DossieLog.action.like(f'%{lname}%'))
+
+    if full_fname :
+        search_term = r'\y{}\y'.format(re.escape(full_fname))
+        filter_conditions.append(DossieLog.action.op('~')(search_term))
+    if full_mname :
+        search_term = r'\y{}\y'.format(re.escape(full_mname))
+        filter_conditions.append(DossieLog.action.op('~')(search_term))
+    if full_lname :
+        search_term = r'\y{}\y'.format(re.escape(full_lname))
+        filter_conditions.append(DossieLog.action.op('~')(search_term))
 
     # Combine filter conditions using AND
     combined_filter = and_(*filter_conditions)
@@ -186,5 +222,65 @@ async def get_dossie_fullname_log_entries(lname: str = Query(None),
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to read Excel file: {e}")
     logging.info(f"{lname} {fname} {mname}", extra={'user': current_user,'table': 'dossie_log','action': 'скачал поиск фл по фио'})
+    return Response(content, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    headers={"Content-Disposition": "attachment;filename=data.xlsx"})
+
+
+
+@router.get("/log/download/search_fio/")
+async def get_log_fullname_log_entries(lname: str = Query(None),
+                                          mname : str = Query(None),
+                                          fname : str = Query(None),
+                                          full_lname: str = Query(None),
+                                          full_mname: str = Query(None),
+                                          full_fname: str = Query(None),
+                                          current_user : str = Depends(get_current_user),
+                                          db : Session = Depends(get_db)):
+
+    # empty list to store filter conditions
+    filter_conditions = []
+    if fname:
+        filter_conditions.append(cast(Log.request_body, String).contains(fname))
+    if mname:
+        filter_conditions.append(cast(Log.request_body, String).contains(mname))
+    if lname:
+        filter_conditions.append(cast(Log.request_body, String).contains(lname))
+
+    if full_fname :
+        search_term = r'\y{}\y'.format(re.escape(full_fname))
+        filter_conditions.append(cast(Log.request_body, String).op('~')(search_term))
+    if full_mname :
+        search_term = r'\y{}\y'.format(re.escape(full_mname))
+        filter_conditions.append(cast(Log.request_body, String).op('~')(search_term))
+    if full_lname :
+        search_term = r'\y{}\y'.format(re.escape(full_lname))
+        filter_conditions.append(cast(Log.request_body, String).op('~')(search_term))
+
+    # Combine filter conditions using AND
+    combined_filter = and_(*filter_conditions)
+
+    log_entries = (db.query(Log.username, User.email, Log.request_body, Log.request_rels, Log.date,Log.approvement_data, Log.obwii, Log.depth_, Log.limit_)
+                       .join(User, Log.username == User.username)
+                                 .filter(combined_filter)
+                                 .order_by(Log.date.desc()).all())
+
+    log_entries_as_dict = [
+        dict(
+            username=row[0], email=row[1], request_body=row[2], request_rels=row[3],
+            date=row[4], approvement_data=row[5], obwii=row[6], depth_=row[7], limit_=row[8]
+        )
+        for row in log_entries
+    ]
+    for entry in log_entries_as_dict:
+        entry['date'] = entry['date'].strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        df = pd.DataFrame(log_entries_as_dict)
+        excel_filename = "data.xlsx"
+        df.to_excel(excel_filename, index=False)
+        with open(excel_filename, "rb") as file:
+            content = file.read()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to read Excel file: {e}")
+
     return Response(content, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                     headers={"Content-Disposition": "attachment;filename=data.xlsx"})
