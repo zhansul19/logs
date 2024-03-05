661a21e3400894866da708d20ab97a001d3f6820
zhansul19 <zhansultanjan@gmail.com>
2024-03-05 16:07:06 +0500

    bug fix

--- update_server.sh
+++ update_server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Stop the backend server
-tmux send-keys -t oculus_backend C-c
+tmux send-keys -t oculus-back C-c
 
 # Pull changes from the Git repository
 cd /root/log_new/logs
 git pull
 
 # Attach to the tmux session and start uvicorn
-tmux attach-session -t oculus_backend -d
-tmux send-keys -t oculus_backend "uvicorn main:app --host 192.168.30.24 --port 5220" C-m
+tmux attach-session -t oculus-back -d
+tmux send-keys -t oculus-back "uvicorn main:app --host 192.168.30.24 --port 5220" C-m
