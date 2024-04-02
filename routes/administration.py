77c9db37cd750510908eab79996b90e4c6b0e327
zhansul19 <zhansultanjan@gmail.com>
2024-04-02 16:06:08 +0500

    fix

--- routes/administration.py
+++ routes/administration.py
@@ -111,41 +111,39 @@ async def get_risk_itab_log_entries(value: str,
         raise HTTPException(status_code=404, detail="User's log entries not found")
 
     return log_entries_as_dict
-from sqlalchemy import or_
 
 
-# @router.get("/risk/dossie_log/{tag}={value}/")
-# async def get_log_entries(value: str,
-#                           tag: str,
-#                           current_user: str = Depends(get_current_user),
-#                           db: Session = Depends(get_db2)):
-#     # Read the CSV file and extract ИИН values
-#     iin_values = []
-#     with open('C:/Users/User6/Desktop/logs/log/administration2.csv', 'r', encoding='utf-8-sig') as file:
-#         csv_reader = csv.reader(file)
-#         next(csv_reader)  # Skip the header row
-#         for row in csv_reader:
-#             iin_values.append(row[3])  # Assuming ИИН values are in the 4th column (index 3)
-#
-#     source = "dossie"
-#
-#     # Constructing multiple like conditions
-#     like_conditions = [DossieLog.action.like(f'%{iin}%') for iin in iin_values]
-#
-#     # Combining the like conditions using or_
-#     combined_condition = or_(*like_conditions)
-#
-#     # Querying DossieLog entries
-#     log_entries = db.query(DossieLog).filter(DossieLog.action.like('Поиск%')).filter(combined_condition).all()
-#
-#     log_entries_as_dict = [
-#         dict(
-#             date=row.log_time, username=row.user_name, iin=row.action, fio=row.lname, source=source
-#         )
-#         for row in log_entries
-#     ]
-#
-#     if not log_entries_as_dict:
-#         raise HTTPException(status_code=404, detail="User's log entries not found")
-#
-#     return log_entries_as_dict
+@router.get("/risks/dossie_log/iin={value}/")
+async def get_log_entries(value: str,
+                          current_user: str = Depends(get_current_user),
+                          db: Session = Depends(get_db2)):
+    # Read the CSV file and extract ИИН values
+    iin_values = []
+    fioval=[]
+    with open('C:/Users/User6/Desktop/logs/log/administration2.csv', 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        next(csv_reader)  # Skip the header row
+        for row in csv_reader:
+            for key, val in row.items():
+                if value in val:
+                    iin_values.append(value)
+                    fioval.append(row['ИИН'])
+                    fioval.append(row['ФИО'])
+                    break
+
+    if iin_values[0] == value:
+        like_conditions = DossieLog.action.like(f'%{value}%')
+
+    log_entries = db.query(DossieLog).filter(DossieLog.action.like('Поиск%')).filter(like_conditions).all()
+
+    log_entries_as_dict = [
+        dict(
+            log_time=row.log_time, user_name=row.lname+" "+row.fname, iin=fioval[0], fio=fioval[1]
+        )
+        for row in log_entries
+    ]
+
+    if not log_entries_as_dict:
+        raise HTTPException(status_code=404, detail="User's log entries not found")
+
+    return log_entries_as_dict
