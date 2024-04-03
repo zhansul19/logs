1a14d0e731249190cf1696d89ad1dabce6363638
zhansul19 <zhansultanjan@gmail.com>
2024-04-03 11:43:49 +0500

    fix

--- routes/administration.py
+++ routes/administration.py
@@ -7,10 +7,10 @@ from sqlalchemy import or_
 
 router = APIRouter()
 
+
 @router.get("/risks/users_log/")
-async def get_risk_entries(request_name: str,
-                          current_user: str = Depends(get_current_user),
-                          db: Session = Depends(get_db)):
+async def get_risk_entries(current_user: str = Depends(get_current_user),
+                           db: Session = Depends(get_db)):
 
     log_entries = (
         db.query(UsersLog.time, UsersLog.username, Administration.iin, Administration.fio)
@@ -31,14 +31,12 @@ async def get_risk_entries(request_name: str,
 
 
 @router.get("/risks/log/")
-async def get_risk_entries(request_name: str,
-                          current_user: str = Depends(get_current_user),
-                          db: Session = Depends(get_db)):
+async def get_risk_entries(current_user: str = Depends(get_current_user),
+                           db: Session = Depends(get_db)):
     log_entries = (
         db.query(Log.date, Log.username, Administration.iin, Administration.fio)
         .join(Administration, Log.obwii.like('%' + Administration.iin + '%'))
     ).all()
-
 
     log_entries_as_dict = [
         dict(
@@ -119,8 +117,9 @@ async def get_log_entries(value: str,
                           db: Session = Depends(get_db2)):
     # Read the CSV file and extract ИИН values
     iin_values = []
-    fioval=[]
-    with open('C:/Users/User6/Desktop/logs/log/administration2.csv', 'r', encoding='utf-8') as file:
+    fioval = []
+    # with open('C:/Users/User6/Desktop/logs/log/administration2.csv', 'r', encoding='utf-8') as file:
+    with open('root/log_new/logs/administration2.csv', 'r', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
         next(csv_reader)  # Skip the header row
         for row in csv_reader:
@@ -142,6 +141,49 @@ async def get_log_entries(value: str,
         )
         for row in log_entries
     ]
+
+    if not log_entries_as_dict:
+        raise HTTPException(status_code=404, detail="User's log entries not found")
+
+    return log_entries_as_dict
+
+
+@router.get("/risks/dossie_log/")
+async def get_log_entries(current_user: str = Depends(get_current_user),
+                          db: Session = Depends(get_db2)):
+    # Read the CSV file and extract ИИН values
+    iin_values = []
+    iin_to_fio_mapping = { }
+
+    # with open('C:/Users/User6/Desktop/logs/log/administration2.csv', 'r', encoding='utf-8') as file:
+    with open('root/log_new/logs/administration2.csv', 'r', encoding='utf-8-sig') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            iin_values.append(row[3])
+            iin_to_fio_mapping[row[3]] = row[2]
+
+    # Constructing multiple like conditions
+    like_conditions = [DossieLog.action.like(f'%{iin}%') for iin in iin_values]
+
+    # Combining the like conditions using or_
+    combined_condition = or_(*like_conditions)
+
+    # Querying DossieLog entries
+    log_entries = db.query(DossieLog).filter(DossieLog.action.like('Поиск%')).filter(combined_condition).all()
+
+    log_entries_as_dict = [
+        dict(
+            log_time=row.log_time, user_name=row.lname + " " + row.fname, iin=row.action, fio=""
+        )
+        for row in log_entries
+    ]
+
+    for entry in log_entries_as_dict:
+        for iin, fio in iin_to_fio_mapping.items():
+            if iin in entry["iin"]:
+                entry["fio"] = fio
+                entry["iin"] = iin
 
     if not log_entries_as_dict:
         raise HTTPException(status_code=404, detail="User's log entries not found")
