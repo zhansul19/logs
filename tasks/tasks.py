a8f9db79d744321b8776fdfc2326e6fe4823d4b3
zhansul19 <zhansultanjan@gmail.com>
2024-04-05 16:14:03 +0500

    fix

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -9,7 +9,7 @@ load_dotenv()
 SMTP_HOST = os.getenv("smtp_host")
 SMTP_PORT = 25
 
-celery = Celery('tasks', broker='redis://127.0.0.1:6379')
+celery = Celery('tasks', broker='redis://127.0.0.1:6379/0')
 
 
 def get_email_template(message: str):
