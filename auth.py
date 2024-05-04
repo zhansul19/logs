0508bbf517d97d478a37333ec205dca9e00e5ccf
zhansul19 <zhansultanjan@gmail.com>
2024-05-04 12:07:01 +0500

    time infinte

--- auth.py
+++ auth.py
@@ -68,13 +68,10 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
+
+    to_encode.update({"exp": None})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
